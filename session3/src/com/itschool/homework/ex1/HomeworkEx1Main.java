fb878d5cbed48649fca22c7394695f730a686b5c
LorenaIoana <marianlorena@yahoo.com>
2024-03-07 18:28:49 +1400

    homework

--- session3/src/com/itschool/homework/ex1/HomeworkEx1Main.java
+++ session3/src/com/itschool/homework/ex1/HomeworkEx1Main.java
@@ -4,5 +4,42 @@ public class HomeworkEx1Main {
 
     public static void main(String[] args) {
         // here we will call homework stuff for exercise X
+
+        int a = 7, b =9;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        double c = 10.5, d = 4.5;
+        System.out.println("c + d = " + (c + d));
+        System.out.println("c * d = " + (c * d));
+        System.out.println("c / d = " + (c / d));
+
+        int e = +10;
+        int f = -20;
+        System.out.println(e);
+        System.out.println(f);
+
+        int num = -25;
+        System.out.println(+num);
+        System.out.println(-num);
+
+        boolean isFirstTime = true;
+        boolean isRegistered = false;
+
+        System.out.println( !isFirstTime);
+        System.out.println( !isRegistered);
+        System.out.println( isFirstTime);
+        System.out.println( isRegistered);
+
+
+        System.out.println(a++);
+        System.out.println(++b);
+
+
+
     }
+
 }
